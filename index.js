d8f7bf5e43f3f3899ff4a2bf321c47e662aaa901
zry19 <69317868+zry19@users.noreply.github.com>
2025-11-10 10:37:18 +0700

    EM v3.3.7 index.js

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ client.on('messageCreate', async (message) => {
 
   // ==== STATUS & HELP ====
   if (command === 'status') {
-    message.reply(`✅ Bot aktif sebagai ${client.user.tag}\nVersion: v3.2.5\nGuilds: ${client.guilds.cache.size}`);
+    message.reply(`Bot active ✅ ${client.user.tag}\nVersion: v3.2.5\nGuilds: ${client.guilds.cache.size}`);
   }
 
   if (command === 'help') {
@@ -191,4 +191,5 @@ client.on('messageCreate', async (message) => {
 
 // ==== LOGIN ====
 client.login(process.env.TOKEN);
+
 
